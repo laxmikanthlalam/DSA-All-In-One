ba2864caeb6b3bc5c3e72b16806674b4a14ec78d
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-03-09 19:11:40 +0530

    Create Flodyp1.java

---
+++ Patterns/Flodyp1.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Flodyp1 {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
+    Scanner in=new Scanner(System.in);
+    int n=in.nextInt();
+    int num=1;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=i;j++)
+        {
+            System.out.print(num+" ");
+            num++;
+        }
+        System.out.println();
+    }
+    }
+}
