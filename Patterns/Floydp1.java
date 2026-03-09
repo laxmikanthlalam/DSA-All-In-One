05d471b289c27e0c72d9c21026c74ce359aa7052
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-03-09 19:12:31 +0530

    Rename class Flodyp1 to Floydp1

--- Patterns/Floydp1.java
+++ Patterns/Floydp1.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Flodyp1 {
+public class Floydp1 {
 
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Main. */
