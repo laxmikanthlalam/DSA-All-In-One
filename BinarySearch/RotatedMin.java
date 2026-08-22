5ef31a074692e1279d135c78de5b81a2cfdf3026
laxmikanthlalam <lalamlaxmikanth1@gmail.com>
2026-08-22 18:13:52 +0530

    Create RotatedMin.java

---
+++ BinarySearch/RotatedMin.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findMin(int[] nums) {
+        int l=0,r=nums.length-1,min=0;
+        while(l<r)
+        {
+            int mid=(l+r)/2;
+            if(nums[mid]>nums[r])
+            {
+                l=mid+1;
+            }
+            else
+            {
+                r=mid;
+            }
+        }
+        return nums[l];
+    }
+}
